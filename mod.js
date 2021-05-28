92996bb1bc165f0ff0d2a8a7e66a5b7d44c581a6
“MatheusSArantes” <matheus.sodre.arantes@gmail.com>
2021-05-28 10:47:24 -0300

    Desafio2

---
+++ mod.js
@@ -0,0 +1,18 @@
+let idade = 18
+
+
+if(idade >= 18){
+    console.log("Parabéns, você é maior de idade")
+}
+
+let humano = true
+
+if(idade >= 18 && humano == true){
+    console.log("Parabéns, você é maior de idade")
+}
+
+const aniversario = "Janeiro"
+
+if(aniversario == "Janeiro" || aniversario == "Dezembro"){
+    console.log("Parabéns, você faz aniversario perto do ano novo")
+}
